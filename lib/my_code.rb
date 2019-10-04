ffb86ed33379753665f441898ac4dd18bdf321af
Deanna Kelley <deanna.georges@gmail.com>
2019-10-04 15:40:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def map_to_no_change(source_array)
   nochange_array = []
   i = 0
   while i < source_array.count do
-    negative_array.push(source_array[i] * -1)
+    nochange_array.push(source_array[i])
     i += 1
   end
 nochange_array
