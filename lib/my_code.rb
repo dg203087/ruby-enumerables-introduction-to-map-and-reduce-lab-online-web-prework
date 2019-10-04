ac2db6f208680c2e443e41febe8aff91bc88bcce
Deanna Kelley <deanna.georges@gmail.com>
2019-10-04 16:08:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.count do
-    return false if (#condition is falsy value)
+    return false if (source_array )
     i += 1
   end
 return true 
