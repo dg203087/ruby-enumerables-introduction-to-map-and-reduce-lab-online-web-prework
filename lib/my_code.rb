cc04189cf1602a0cc1522f5baede5aaf230f6d94
Deanna Kelley <deanna.georges@gmail.com>
2019-10-04 16:02:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,3 +47,21 @@ def reduce_to_total(source_array, starting_point=0)
   end
 total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.count do
+    
+    i += 1
+  end
+total
+end
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.count do
+    total += source_array[i]
+    i += 1
+  end
+total
+end
