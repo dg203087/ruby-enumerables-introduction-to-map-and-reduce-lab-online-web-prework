2d3fa2df2b91063d2c6eb5386e5eb5f92135fd1b
Deanna Kelley <deanna.georges@gmail.com>
2019-10-04 15:43:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,4 +28,12 @@ def map_to_double(source_array)
 double_array
 end
 
-
+def map_to_square(source_array)
+  square_array = []
+  i = 0 
+  while i < source_array.count do
+    square_array.push(source_array[i] * 2)
+    i += 1
+  end
+square_array
+end
