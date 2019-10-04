c338fa3eeb374d82392259f34d7c2dcde12f6f74
Deanna Kelley <deanna.georges@gmail.com>
2019-10-04 15:22:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# My Code here....
+def map_to_negativized(source_array)
+  negative_array = []
+  i = 0
+  while i < source_array.size do
+    negative_array.push(source_array[i] * -1)
+    i += 1
+  end
+negative_array
+end
+
+def map_to_no_change(source_array)
+  dune
+end
+
+def map_to_double(source_array)
+  i = 0 
+  double_array = []
+  while i < source_array.count do
+    double_array = (source_array * 2)
+    i += 1
+  end
+double_array
+end
+
+
